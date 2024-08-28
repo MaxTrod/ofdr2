6ee3f74b1da8b3e661561e2ec7c6cb1b9999e2d5
MaxTrod <maximsiminko@gmail.com>
2024-08-28 14:07:16 +0500

    added documents section

--- src/js/modules/_functions.js
+++ src/js/modules/_functions.js
@@ -11,3 +11,59 @@ export function isWebp() {
 		document.documentElement.classList.add(className);
 	});
 }
+
+export function FLS(message) {
+	setTimeout(() => {
+		if (window.FLS) {
+			console.log(message);
+		}
+	}, 0);
+}
+
+/* Перевірка мобільного браузера */
+export let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+export let bodyLockStatus = true;
+
+export let bodyLock = (delay = 500) => {
+	if (bodyLockStatus) {
+		const lockPaddingElements = document.querySelectorAll("[data-lp]")
+		const lockPaddingValue = window.innerWidth - document.body.offsetWidth + 'px'
+		lockPaddingElements.forEach(lockPaddingElement => {
+			lockPaddingElement.style.paddingRight = lockPaddingValue
+		});
+
+		document.body.style.paddingRight = lockPaddingValue
+		document.documentElement.classList.add("lock")
+
+		bodyLockStatus = false
+		setTimeout(function () {
+			bodyLockStatus = true
+		}, delay)
+	}
+}
+
+export let bodyUnlock = (delay = 500) => {
+	if (bodyLockStatus) {
+		const lockPaddingElements = document.querySelectorAll("[data-lp]");
+		setTimeout(() => {
+			lockPaddingElements.forEach(lockPaddingElement => {
+				lockPaddingElement.style.paddingRight = ''
+			});
+			document.body.style.paddingRight = ''
+			document.documentElement.classList.remove("lock")
+		}, delay)
+		bodyLockStatus = false
+		setTimeout(function () {
+			bodyLockStatus = true
+		}, delay)
+	}
+}
+
+export let bodyLockToggle = (delay = 500) => {
+	if (document.documentElement.classList.contains('lock')) {
+		bodyUnlock(delay)
+	} else {
+		bodyLock(delay)
+	}
+}
